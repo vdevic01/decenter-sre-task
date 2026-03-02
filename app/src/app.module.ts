33f61bb4df928a989f2cc93741450550202400ba
vdevic01 <vlada.devic.2001@gmail.com>
2026-03-02 20:58:03 +0100

    feat: Health check

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), HealthModule],
   controllers: [],
   providers: [],
 })
